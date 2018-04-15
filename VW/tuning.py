d16e616323836f32036916b8dce1797f1ee3202b
Sheldon Benard <imac@Sheldons-MBP.lan>
2018-04-15 11:13:34 -0400

    Finish tuning

--- VW/tuning.py
+++ VW/tuning.py
@@ -4,10 +4,10 @@
 # vowpal_wabbit/vowpalwabbit/vw --oaa 5 -d ../data/amazon_review_full_csv/train_vw.csv -c --passes 15 --loss_function logistic -b25 --ngram 2 --learning_rate 0.2 --skips 1 --holdout_period 3 -f model.vw
 # vowpal_wabbit/vowpalwabbit/vw -t ../data/amazon_review_full_csv/test_vw.csv -i model.vw -p pred.txt
 
-# vowpal_wabbit/vowpalwabbit/vw --oaa 2 -d ../data/amazon_review_polarity_csv/train_vw.csv --learning_rate 0.8 --loss_function logistic -b25 --ngram 2 --skips 1 -f model.vw
+# vowpal_wabbit/vowpalwabbit/vw --oaa 2 -d ../data/amazon_review_polarity_csv/train_vw.csv -c --passes 15 --holdout_period 3 --learning_rate 0.3 --loss_function logistic -b25 --ngram 2 --skips 1 -f model.vw
 # vowpal_wabbit/vowpalwabbit/vw -t ../data/amazon_review_polarity_csv/test_vw.csv -i model.vw -p pred.txt
 
-# vowpal_wabbit/vowpalwabbit/vw --oaa 14 -d ../data/dbpedia_csv/train_vw.csv --loss_function logistic -b25 --learning_rate 0.9 --ngram 2 --skips 1 -f model.vw
+# vowpal_wabbit/vowpalwabbit/vw --oaa 14 -d ../data/dbpedia_csv/train_vw.csv -c --loss_function logistic -b25 --passes 15 --holdout_period 3 --learning_rate 0.5 --ngram 2 --skips 1 -f model.vw
 # vowpal_wabbit/vowpalwabbit/vw -t ../data/dbpedia_csv/test_vw.csv -i model.vw -p pred.txt
 
 # vowpal_wabbit/vowpalwabbit/vw --oaa 5 -d ../data/sogou_news_csv/train_vw.csv --loss_function logistic -b25 --ngram 2 --skips 1 -f model.vw
@@ -59,5 +59,5 @@ def hyper_parameter_tuning(classes, dataset):
 # hyper_parameter_tuning(14,"dbpedia_csv")
 # hyper_parameter_tuning(2,"amazon_review_polarity_csv")
 # hyper_parameter_tuning(5,"amazon_review_full_csv")
-hyper_parameter_tuning(5,"sogou_news_csv")
+# hyper_parameter_tuning(5,"sogou_news_csv")
 hyper_parameter_tuning(10,"yahoo_answers_csv")
